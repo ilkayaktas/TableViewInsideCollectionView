01527d1a6fc5e515869c36f234ac0a46d94a0db6
İlkay Aktaş <ilkayaktas@gmail.com>
2019-10-17 22:33:30 +0300

    Initial Commit

---
+++ TableViewInsideCollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewInsideCollectionView
+//
+//  Created by İlkay Aktaş on 17.10.2019.
+//  Copyright © 2019 İlkay Aktaş. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
